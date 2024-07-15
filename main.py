45f813826966a8785b9cf11499c0d76c162ae433
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-07-15 13:33:20 +0800

    feat: updated main script to run the multiple image analysis function

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-# script to download objects from bucket
+# run main.py to analyse images
 
-from s3_object_download import download_s3_bucket
-
-download_s3_bucket()
-
-# script to analyse single image
-
-from single_image_analysis import single_image_analysis
-
-single_image_analysis()
-
-# script to analyse batch of images
+# if you need to download images, run s3_object_download.py - ensure that variable names have been entered
 
 from multiple_image_analysis import multiple_image_analysis
 
 multiple_image_analysis()
-
-# script to stitch images
-
-from image_stitch import image_stitch
-
-image_stitch()
